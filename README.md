517d7e79513b89483f5135a7d51daafd7fe5ee9c
Jake Thrasher <mjakethrasher@gmail.com>
2021-01-13 14:28:59 -0800

    initial HTML structure and js

--- README.md
+++ README.md
@@ -1,1 +1,15 @@
 # lab-03-rock-paper-scissors
+## Rules: 
+(paper beats rock, rock beats scissors, scissors beats papers)
+## JAVASCRIPT
+ * grab dom elements
+ * initialize state
+   * wins
+   * num of plays
+* event listener
+    * 
+    * 
+
+* utils.js
+    * getRandomThrow
+    * doesUserWin
--- app.js
+++ app.js
@@ -1,5 +1,24 @@
-// import functions and grab DOM elements
+import { getRandomThrow, didUserWin} from './get-random-throw.js';
+
+const playButton = document.getElementById('play');
+const throwResult = document.getElementById('throw-result');
+const winsSpan = document.getElementById('wins-span');
+const lossesSpan = document.getElementById('losses-span');
+const drawsSpan = document.getElementById('draws-span');
+const whoWon = document.getElementById('who-won');
 
 // initialize state
+let wins=0;
+let totalPlays = 0;
 
 // set event listeners to update state and DOM
+playButton.addEventListener('click', () =>{
+    totalPlays++;
+    console.log(totalPlays)
+    //increment totalPlays
+    //import getRandomThrow which returns rock paper or sciccors
+    //import didUserWin which compares computer throw to user throw
+        //user throw is input.value
+    //if user wins increment wins
+    //display who won and change textContent of wins losses and draws span
+})
